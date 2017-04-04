a65dde738e2332df68bcbd71f73b135c0ad31ba8
jimjones26 <jimjones26@gmail.com>
2017-04-04 15:37:43 -0400

    add complete / delete functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TODO List App';
-  tasks: Array<string> = [];
+  tasks: Array<Object> = [];
   buttonLabel = 'Add Task';
 
   addTask(taskInput: string) {
-    this.tasks.push(taskInput);
+    this.tasks.push({
+      label: taskInput,
+      isComplete: false
+    });
     console.log(this.tasks);
+  }
+
+  completeTask(index: number) {
+    console.log('completing', this.tasks[index]);
+    const taskToComplete: any = this.tasks[index];
+    taskToComplete.isComplete = !taskToComplete.isComplete;
+  }
+
+  deleteTask(index: number) {
+    console.log(index);
+    this.tasks.splice(index, 1);
   }
 }
